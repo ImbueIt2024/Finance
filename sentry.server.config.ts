57802ac37ea3cb268ff346888193df2d4cd35f28
ImbueIt2024 <158772631+ImbueIt2024@users.noreply.github.com>
2024-07-25 16:55:29 -0400

    Add files via upload

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5eed15d343213fd8554210f5f6e6f95e@o4507609043501056.ingest.us.sentry.io/4507609047236608",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
